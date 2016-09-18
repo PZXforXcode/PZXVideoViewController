4b830bdd5c9df28da2f1dc16d8a63d5eb7ff08ce
彭祖鑫 <496912046@qq.com>
2016-09-18 17:11:29 +0800

    Initial Commit

---
+++ PZXVideoViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PZXVideoViewController
+//
+//  Created by pzx on 16/9/18.
+//  Copyright © 2016年 pzx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PZXVideoViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PZXVideoViewController
+//
+//  Created by pzx on 16/9/18.
+//  Copyright © 2016年 pzx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
